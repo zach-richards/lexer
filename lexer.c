1c0d39e462d762fdc96d38a35a6c0267818d7865
Zach <zacharyryanrichards@gmail.com>
2026-02-27 13:29:19 -0800

    Added support for chars and c-style multi comments

--- lexer.c
+++ lexer.c
@@ -1,78 +1,134 @@
-// x01_file_reader.c
+// lexer.c
 
-// Parses the input file into tokens split by whitespace.
+// Parses the input file into tokens split by whitespace. Can also classify and parse strings, chars and comments,
+// throwing out comments.
 
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, char* argf[]) {
-	FILE *fp = fopen(argf[1], "r"); // read only
-	int isString = 0;
-	
-	// test null file
+int main(int argc, char* argv[]) {
+    	if (argc < 2) {
+        	printf("Usage: %s <filename>\n", argv[0]);
+        	return -1;
+    	}
+
+    	FILE *fp = fopen(argv[1], "r");
+    
 	if (fp == NULL) {
-		printf("Error: Could not open file\n");
-		exit(-1);
-	}
+        	printf("Error: Could not open file\n");
+        	return -1;
+    	}
 
-	// read each char
-	int ch;
-	char sArr[256];
-	int ind = 0;
-	while ((ch = fgetc(fp)) != EOF) {
-		switch (ch) {
-			case ' ':
+    	int ch;
 
-			case '\n':
+    	int isString = 0;
+    	char sArr[256];
+    	int sInd = 0;
 
-				if (isString == 1) {
-					sArr[ind] = ch;
-					ind++;
-				}
+    	int isChar = 0;
+    	char cArr[4];
+    	int cInd = 0;
 
-				break;
+    	int isComment = 0;
+    	char comArr[256];
+    	int comInd = 0;
 
-			case '"': // boolean. First \" sets bool to true, last sets it to false
-				  
-				if (isString == 0) {
-					isString = 1;
-					sArr[0] = '"';
-					
-					ind = 1;
-				} else {
-					isString = 0;
+    	while ((ch = fgetc(fp)) != EOF) {
 
-					sArr[ind] = ch;
-					sArr[ind + 1] = '\0';
-					ind = 0;
-					
-					printf("String: ");
-					for (int i = 0; sArr[i] != '\0'; i++) {
-						 printf("%c", sArr[i]);
+        	if (ch == '"') {
+            		if (!isString) {
+                		isString = 1;
+                		sInd = 0;
+                		sArr[sInd] = ch;
+				sInd++;
+            		} else {
+                		sArr[sInd] = ch;
+				sInd++;
+                		sArr[sInd] = '\0';
+                		printf("String: %s\n", sArr);
+                		isString = 0;
+                		sInd = 0;
+            		}
+            		continue;
+        	}
+
+        	if (ch == '\'') {
+            		if (!isChar && !isString) {
+                		isChar = 1;
+                		cInd = 0;
+                		cArr[cInd] = ch;
+				cInd++;
+           	 	} else if (isChar) {
+                		cArr[cInd] = ch;
+				cInd++;
+                		cArr[cInd] = '\0';
+               	 		printf("Char: %s\n", cArr);
+                		isChar = 0;
+            		} else if (isString) {
+                		sArr[sInd] = ch;
+				sInd++;
+            		}
+            		continue;
+        	}
+
+		if (ch == '/' && !isString && !isChar) {
+		    	int next = fgetc(fp);
+
+			if (next == '*') {
+		    		isComment = 1;
+		    		comInd = 0;
+		    		comArr[comInd] = '/';
+				comInd++;
+		    		comArr[comInd] = '*';
+				comInd++;
+
+		    		int prev = 0;
+
+		    		while ((ch = fgetc(fp)) != EOF) {
+					comArr[comInd] = ch;
+					comInd++;
+
+					if (prev == '*' && ch == '/') {
+			    			break;
 					}
-					printf("\n");
-				}
+					prev = ch;
+		    		}
 
-				break;
-
-			default:
-
-				if (isString == 1){
-					sArr[ind] = ch;
-					ind++;
-				} else {
-					printf("Token: ");
-					while (ch != '\n' && !isspace(ch)) { // parse by whitespace and new line
-						printf("%c", ch);
-						ch = fgetc(fp);
-					}
-					printf("\n");
-				}
-
-				break;
+		    		comArr[comInd] = '\0';
+		    		isComment = 0;
+		    		continue;
+		    	} else {
+				ungetc(next, fp);
+			}
 		}
-	}
-	
-	return 0;
+
+		if (isComment) {
+			continue;
+		}
+
+        	if (isString) {
+            		sArr[sInd++] = ch;
+            		continue;
+        	}
+
+        	if (isChar) {
+            		cArr[cInd++] = ch;
+            		continue;
+        	}
+
+        	if (!isspace(ch)) {
+            		printf("Token: ");
+            	do {
+                	printf("%c", ch);
+                	ch = fgetc(fp);
+            	} while (ch != EOF && !isspace(ch) && ch != '"' && ch != '\'');
+            	printf("\n");
+            	if (ch == EOF) break;
+            		ungetc(ch, fp);
+        	}
+    	}
+
+    	fclose(fp);
+    	return 0;
 }
