00862115f6a683260d4d0d94ad30471144d2399d
Zach <zacharyryanrichards@gmail.com>
2026-03-05 09:54:08 -0800

    Added support for keywords and operators

--- lexer.c
+++ lexer.c
@@ -7,128 +7,218 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc, char* argv[]) {
-    	if (argc < 2) {
-        	printf("Usage: %s <filename>\n", argv[0]);
-        	return -1;
+// List of operators
+const char *operators[] = {
+	"{:", "}:", ":=", "..", "<<", ">>", "<>", "<=", ">=", "**", "==", "!=", "=>", "<", ">", "(", ")", "+", 
+	"-", "*", "/", "|", "&", ";", ",", ":", "=", "$", "@", "[", "]", "{", "}", "."
+};
+
+const int NUM_OF_OPS = 34;
+
+// List of keywords
+const char *keywords[] = { 
+	"accessor", "character", "constant", "mutator", "natural", "positive", "subtype", "integer", 
+	"format", "elsif", "others", "while", "exit", "float", "array", "accessor", "proc", "then", 
+	"type", "func", "ptr", "sort", "plus", "case", "else", "ifc", "null", "out", "pkg", "or", 
+	"in", "is", "if", "of", "when" 
+}; 
+
+const int NUM_OF_KEYWORDS = 35;
+
+int indentation = 0;
+
+// indent for functions and loops
+void indent(int indentation) {
+        for (int i = 0; i < indentation; i++) {
+                printf("  ");
+        }
+}
+
+// check for operators and if needed, print operator
+int checkForOps(FILE *fp, int ch) {
+
+    if (ch == '}') { indentation--; }
+
+    for (int i = 0; i < NUM_OF_OPS; i++) {
+
+        int opLen = 0;
+        while (operators[i][opLen] != '\0') opLen++;
+
+        if (opLen == 2) {
+
+            int next = fgetc(fp);
+
+            if (ch == operators[i][0] && next == operators[i][1]) {
+                indent(indentation);
+                printf("Operator: %s\n", operators[i]);
+		if (ch == '{') { indentation++; }
+                return 1;
+            }
+
+            ungetc(next, fp);
+        }
+
+        else if (opLen == 1 && ch == operators[i][0]) {
+            indent(indentation);
+            printf("Operator: %s\n", operators[i]);
+	    if (ch == '{') { indentation++; }
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    	int i = 0;
+
+    	// Keep looping as long as characters are the same
+    	while (s1[i] == s2[i]) {
+        	if (s1[i] == '\0') {
+            		return 0; // Both strings ended at the same time; they are equal
+        	}
+        	i++;
     	}
 
-    	FILE *fp = fopen(argv[1], "r");
-    
-	if (fp == NULL) {
-        	printf("Error: Could not open file\n");
-        	return -1;
-    	}
+    	// Return the difference between the two differing characters
+    	return (unsigned char)s1[i] - (unsigned char)s2[i];
+}
 
-    	int ch;
+int main(int argc, char* argv[]) { 
+	if (argc < 2) {
+		printf("Usage: %s <filename>\n", argv[0]); 
+		return -1; 
+	} 
+	
+	FILE *fp = fopen(argv[1], "r");
+	
+	if (fp == NULL) { 
+		printf("Error: Could not open file\n"); 
+		return -1; 
+	} 
+	
+	int ch; 
+	int isString = 0; 
+	char sArr[256]; 
+	int sInd = 0; 
+	
+	int isChar = 0; 
+	char cArr[4]; 
+	int cInd = 0; 
+	
+	int isComment = 0; 
+	char comArr[256]; 
+	int comInd = 0; 
+	
+	int isKeyword = 0; 
+	int isOperator = 0;
 
-    	int isString = 0;
-    	char sArr[256];
-    	int sInd = 0;
+	while ((ch = fgetc(fp)) != EOF) {
+	    	if (isspace(ch)) continue;
 
-    	int isChar = 0;
-    	char cArr[4];
-    	int cInd = 0;
-
-    	int isComment = 0;
-    	char comArr[256];
-    	int comInd = 0;
-
-    	while ((ch = fgetc(fp)) != EOF) {
-
-        	if (ch == '"') {
-            		if (!isString) {
-                		isString = 1;
-                		sInd = 0;
-                		sArr[sInd] = ch;
-				sInd++;
-            		} else {
-                		sArr[sInd] = ch;
-				sInd++;
-                		sArr[sInd] = '\0';
-                		printf("String: %s\n", sArr);
-                		isString = 0;
-                		sInd = 0;
-            		}
-            		continue;
-        	}
-
-        	if (ch == '\'') {
-            		if (!isChar && !isString) {
-                		isChar = 1;
-                		cInd = 0;
-                		cArr[cInd] = ch;
-				cInd++;
-           	 	} else if (isChar) {
-                		cArr[cInd] = ch;
-				cInd++;
-                		cArr[cInd] = '\0';
-               	 		printf("Char: %s\n", cArr);
-                		isChar = 0;
-            		} else if (isString) {
-                		sArr[sInd] = ch;
-				sInd++;
-            		}
-            		continue;
-        	}
-
-		if (ch == '/' && !isChar) {
-		    	int next = fgetc(fp);
-
+	    	// handle Comments
+	    	if (ch == '/') {
+			int next = fgetc(fp);
 			if (next == '*') {
-		    		isComment = 1;
-		    		comInd = 0;
-		    		comArr[comInd] = '/';
-				comInd++;
-		    		comArr[comInd] = '*';
-				comInd++;
-
+		    		// Read until "*/" is found
 		    		int prev = 0;
-
 		    		while ((ch = fgetc(fp)) != EOF) {
-					comArr[comInd] = ch;
-					comInd++;
-
-					if (prev == '*' && ch == '/') {
-			    			break;
-					}
+					if (prev == '*' && ch == '/') break;
 					prev = ch;
 		    		}
-
-		    		comArr[comInd] = '\0';
-		    		isComment = 0;
-		    		continue;
-		    	} else {
-				ungetc(next, fp);
+		    		continue; 
+			} else {
+		    		ungetc(next, fp);
 			}
-		}
+	    	}
 
-		if (isComment) {
+	    	// Handle Strings
+	    	if (ch == '"') {
+			indent(indentation);
+			printf("String: ");
+			while ((ch = fgetc(fp)) != EOF && ch != '"') {
+		    		putchar(ch);
+			}
+			printf("\n");
 			continue;
+	    	}
+
+	    	// Handle Keywords
+	    	if (isalpha(ch)) {
+			char temp[64];
+			int i = 0;
+			temp[i++] = (char)ch;
+		
+			while (i < 63) {
+		    		int next = fgetc(fp);
+		    		if (next == EOF || (!isalnum(next) && next != '_')) {
+					ungetc(next, fp);
+					break;
+		    		}
+		    		temp[i++] = (char)next;
+			}
+			temp[i] = '\0';
+
+			int foundKeyword = 0;
+			for (int j = 0; j < NUM_OF_KEYWORDS; j++) {
+		    		if (strcmp(temp, keywords[j]) == 0) {
+					indent(indentation);
+					printf("Keyword: %s\n", temp);
+					foundKeyword = 1;
+					break;
+		    		}
+			}
+			if (!foundKeyword) {
+				indent(indentation);
+				printf("Token: %s\n", temp);
+			}
+
+			continue; 
+	    	}
+
+		// Default to token
+		int matched = 0;
+
+		if (!isspace(ch)) {
+
+			if (checkForOps(fp, ch)) {
+				matched = 1;
+			}
+
+			char temp[256];
+			int i = 0;
+
+			while (ch != EOF && !isspace(ch) && matched == 0) {
+
+				temp[i++] = (char)ch;
+
+				int next = fgetc(fp);
+
+				int isOpStart = 0;
+				for (int j = 0; j < NUM_OF_OPS; j++) {
+					if (operators[j][0] == next) {
+						isOpStart = 1;
+						break;
+					}	
+				}
+
+				if (isOpStart) {
+					ungetc(next, fp);
+					break;
+				}
+
+				ch = next;
+			}
+
+			temp[i] = '\0';
+
+			if (i > 0) {
+				indent(indentation);
+				printf("Token: %s\n", temp);
+			}	
 		}
-
-        	if (isString) {
-            		sArr[sInd++] = ch;
-            		continue;
-        	}
-
-        	if (isChar) {
-            		cArr[cInd++] = ch;
-            		continue;
-        	}
-
-        	if (!isspace(ch)) {
-            		printf("Token: ");
-            	do {
-                	printf("%c", ch);
-                	ch = fgetc(fp);
-            	} while (ch != EOF && !isspace(ch) && ch != '"' && ch != '\'');
-            	printf("\n");
-            	if (ch == EOF) break;
-            		ungetc(ch, fp);
-        	}
-    	}
-
-    	fclose(fp);
-    	return 0;
+	}	
+	
+	fclose(fp); 
+	return 0; 
 }
